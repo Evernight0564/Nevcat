3e8b4b56fe1f6d317a437df8dc74e7deca49f7ce
Evernight0564 <cherrybellsama@gmail.com>
2025-11-17 02:18:52 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+let songs = [
+    {
+        name: 'I ﹤3 YOU',
+        path: 'musics/Song 1.mp3',
+        artist: 'MARINA',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'Angel',
+        path: 'musics/Song 2.mp3',
+        artist: 'NewDad',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'headlock',
+        path: 'musics/Song 3.mp3',
+        artist: 'Imogen Heap',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: "Taking What's Not Yours",
+        path: 'musics/Song 4.mp3',
+        artist: 'TV Girl',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: "I Can't Decide",
+        path: 'musics/Song 5.mp3',
+        artist: 'Scissor Sisters',
+        cover: 'images/cover 5.png'
+    },
+    {
+        name: 'Loop (ฉันจึงวนกลับมา)',
+        path: 'musics/Song 6.mp3',
+        artist: 'Asia7',
+        cover: 'images/cover 6.png'
+    },
+]
